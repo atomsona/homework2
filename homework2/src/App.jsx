1c9cda78bf7e84cff4eb73a62cfc8330bc6b998f
gio <giozedelashvili@gmail.com>
2024-10-04 16:10:05 +0400

    test

---
+++ homework2/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Card from './components/card/Card'
+
+function App() {
+  
+
+  return (
+    <div>
+    <Card name={"luka"} age={"13"} />
+    <Card  name={"giorgi"} age={"15"} />
+    <Card name={"mariami"} age={"16"} />
+    <Card name={"nikolozi"} age={"14"} />
+    </div>
+  )
+}
+
+export default App
